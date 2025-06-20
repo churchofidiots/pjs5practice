f377035beded2884ce55cd041c6627c1f2de7954
churchofidiots <wolfnamiko@gmail.com>
2025-06-20 11:51:26 -0700

    work pls?

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ function setup()
 {
     createCanvas(400,400);
     background(220);
+    point(150,250);
+    line(0,0,400,400);
 }
 
 function draw()
 {
-    point(150,250);
-    line(0,0,400,400);
+    
 }
